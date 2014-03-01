f06ea251cd3911f400d576749b3c40e4266bceba
Eric Lou <lwc_evale@hotmail.com>
2014-03-02 00:15:19 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Piaofang::Application.config.secret_key_base = 'f319ccf07761b5e98fd48bb6ed172ffc735fe3da8dae11d2ea59a9529d1079c15e7135e18dc860413b492d83d8dacf11e2ff87de143949927626920766707fff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Piaofang::Application.config.session_store :cookie_store, key: '_piaofang_session'
