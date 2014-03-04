e8ac4836c8df3b8a16ce25f012df5a2db0bacfdc
sunday35034 <lwc_evale@hotmail.com>
2014-03-04 18:13:48 +0800

    完成部分脚本

--- scripts/grab_boxoffice_from_mtime.rb
+++ scripts/grab_boxoffice_from_mtime.rb
@@ -4,21 +4,38 @@ require "nokogiri"
 
 BASE_URL = "http://movie.mtime.com/boxoffice"
 
-def area_list
-  [
-    ['US', '北美票房榜'],
-    ['HK', '香港票房榜'],
-    ['CN', '内地票房榜'],
-    ['TW', '台湾票房榜'],
-    ['JP', '日本票房榜'],
-    ['KR', '韩国票房榜'],
-    ['FR', '法国票房榜']
-  ]
-end
 
 html_stream = open(BASE_URL)
 page = Nokogiri::HTML(html_stream)
 
-page.css("div.ticket_list").collect do |boxoffice_item|
-  p 1
+page.css('div.ticket_list').collect do |area_item|
+
+  area_item.css('ul.content li').collect do |row|
+    # prepare row data
+    rid = row.css('em.c_green').text # 排名
+    name = row.css('a').text.strip # 名称
+    year = row.css('em.c_666').text # 年份
+    area = area_item['name'] # 区域
+    wk = area_item['date'] # 周数
+    wboxoffice = row.css('span.weekly').text # 周票房
+    tboxoffice = row.css('span.sum').text # 总票房
+
+    p rid
+    p name
+    p year
+    p area
+    p wk
+    p wboxoffice
+    p tboxoffice
+
+    # grab img_url, director, actor_list from detail page
+    detail_url = row.css('a').attr('href').text
+    detail_stream = open(detail_url)
+    detail_page = Nokogiri::HTML(detail_stream)
+
+    image_url = detail_page.css('img.movie_film_img').attr('src').text
+    p image_url
+
+  end
+
 end
