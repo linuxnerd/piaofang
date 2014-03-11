8d403ef794264e1a88832e113f47e9cc0b0d964d
Eric Lou <lwc_evale@hotmail.com>
2014-03-11 22:09:57 +0800

    完成票房榜查询和展现

--- app/controllers/honor_controller.rb
+++ app/controllers/honor_controller.rb
@@ -1,22 +1,12 @@
 class HonorController < ApplicationController
   def search
     unless params['movie_name'].blank?
-      movie_list = Movie.where("name like ? or en_name like ?",
+      @boxoffice_movies_list = Movie.where("name like ? or en_name like ?",
           "%#{params['movie_name']}%", "%#{params['movie_name']}%")
-      if movie_list
-        @bhonor_list = []
-        movie_list.collect do |movie|
-          # 根据名称去票房榜中查找列表
-          movie.boxoffices.collect do |b|
-            @bhonor_list << { :rid => b.rid, :name => movie.name,
-                              :wk => b.wk, :area => b.area }
-          end
-        end
 
-        # 按照票房榜排名排序
-        @bhonor_list.sort_by!{ |b| b[:rid] }
-        p @bhonor_list
-      end
+      @honor_movies_list = Honor.select(:name).distinct.where("name like ?","%#{params['movie_name']}%")
+
+
       render 'index'
     end
   end
